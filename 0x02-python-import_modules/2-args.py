5c6231e48f78a461ea465ba4f2f7d01a658c08de
jumbamark <jumbamark@yahoo.com>
2022-06-03 01:41:45 +0000

    fixed styling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(len(argv[1:])))
 
     for i in argv[1:]:
-        print("{}: {}".format((argv[1:].index(i)) +1, i))
+        print("{}: {}".format((argv[1:].index(i)) + 1, i))
