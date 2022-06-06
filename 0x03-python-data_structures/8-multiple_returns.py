920fda6f58a9bec16b7cdb4c95ac86cc51ac9cdb
jumbamark <jumbamark@yahoo.com>
2022-06-06 15:35:34 +0000

    more returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+def multiple_returns(sentence):
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    my_tuple = len(sentence), sentence[0]
+    return my_tuple
