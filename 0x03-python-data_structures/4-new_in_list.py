445568b3a31299fd0efa950cf36a24b4ea1f1847
jumbamark <jumbamark@yahoo.com>
2022-06-06 10:06:11 +0000

    added copy of list in a variable

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
+    new_list = my_list.copy()
+    if idx < 0 or idx > len(new_list) - 1:
         return None
-    my_list[idx] = element
-    return my_list.copy
+    new_list[idx] = element
+    return new_list
