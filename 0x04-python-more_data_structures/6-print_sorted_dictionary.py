870894f9af319865183c2c3fbfb5b57cbb5be447
jumbamark <jumbamark@yahoo.com>
2022-06-08 13:28:43 +0000

    print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = []
+    for key in a_dictionary:
+        keys.append(key)
+    keys.sort()
+    new_dict = {}
+    for i in keys:
+        new_dict[i] = a_dictionary[i]
+    #print(new_dict)
+    for m, n in new_dict.items():
+        print(f"{m}: {n}")
