d275bbcf97c5f428341724a156c3aef3e587b765
jumbamark <jumbamark@yahoo.com>
2022-06-08 16:54:46 +0000

    Interview:converting roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+def roman_to_int(roman_string):
+    dict_rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
+                "D": 500, "M": 1000}
+
+    i = 0
+    integer = 0
+
+    while (i < len(roman_string)):
+        val_char1 = dict_rom[roman_string[i]]
+        if (i + 1) < len(roman_string):
+            # do something
+            val_char2 = dict_rom[roman_string[i + 1]]
+            if val_char1 >= val_char2:
+                integer = integer + val_char1
+                i += 1
+            else:
+                integer = integer + val_char2 - val_char1
+                i += 2
+        else:
+            integer = integer + val_char1
+            i += 1
+
+    return integer
