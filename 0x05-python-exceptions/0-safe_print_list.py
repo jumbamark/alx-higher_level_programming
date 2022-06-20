eab0b0795fcd080562c47a1746cee6a51fc55987
jumbamark <jumbamark@yahoo.com>
2022-06-20 12:55:55 +0000

    specify exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     iterations = 0
-
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-        except:
+        except Exception:
             break
         else:
             iterations += 1
     print()
-
     return iterations
