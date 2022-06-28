59ba7d69c30f2958f98fc6951a1216fe01159b38
jumbamark <jumbamark@yahoo.com>
2022-06-28 02:18:34 +0000

    string representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Module 3-rectangle
+"""
 Defines a Rectangle class.
 """
 
 
 class Rectangle:
-    """Rectangle class defined by width and height."""
+    """Represents a rectangle with width and height."""
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
