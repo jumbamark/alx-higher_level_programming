bc22ebaee1b78815df47cfcd0b92855bbc4f5243
jumbamark <jumbamark@yahoo.com>
2022-06-29 03:03:59 +0000

    pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 Function that adds 2 integers
 """
 
+
 def add_integer(a, b=98):
     """Adds two integers and/or float numbers
 
@@ -19,7 +20,7 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if isinstance(b, int) is False and  isinstance(b, float) is False:
+    if isinstance(b, int) is False and isinstance(b, float) is False:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
