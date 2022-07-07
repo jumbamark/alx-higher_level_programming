c8ca1b0d0e9886cba650bbbe3e9956461757d50e
jumbamark <jumbamark@yahoo.com>
2022-07-07 04:22:49 +0000

    Technical Interview Preparation: Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Modules that returns a list of lists of integers representing
+the Pascal's triangle of ``n``.
+"""
+
+
+def pascal triangle(n):
+    """Function that returns the Pascal triangle n.
+
+    Args:
+        n: size of the triangle (rows)
+
+    Returns:
+        matrix: a matrix with the pascal triangle
+
+    """
+    if n <= 0:
+        return []
+
+    rows = [[1 for j in range(i + 1)] for i in range(n)]
+    for n in range(n):
+        for i in range(n - 1):
+            rows[n][i + 1] = sum(rows[n - 1][i:1 + 2])
+    return rows
