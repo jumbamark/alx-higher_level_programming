716d86a0279b5d54112403a0acbd0b525c32a564
jumbamark <jumbamark@yahoo.com>
2022-07-07 03:04:37 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,21 +7,22 @@ Use your function ``load_from_json_file`` from ``6-load_from_json_file.p``y
 """
 
 import sys
+import json
 import os.path
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
+my_file = 'add_item.json'
 
-filename = "add_item.json"
 my_list = []
 
-if os.path.exists(filename) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(filename)
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
 
-    if len(sys.argv) > 1:
-        for arg in sys.argv[1:]:
-            my_list.append(arg)
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
 
-save_to_json_file(my_list, filename)
+save_to_json_file(my_list, my_file)
