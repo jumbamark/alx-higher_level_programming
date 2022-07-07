fbfbc12c1be77ee5f2cf26ffba7132591acb59e5
jumbamark <jumbamark@yahoo.com>
2022-07-07 01:44:58 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+"""
+Module that contains a function that reads from a text file (UTF8)
+"""
+
+
+def read_file(filename=""):
+    """Reads from filename and prints its contents to stdout.
+
+    Args:
+        filename: name of the file
+
+    """
+
+    with open(filename, encoding="utf-8", mode="r") as f:
+        read_text = f.read()
+        print(read_text, end="")
