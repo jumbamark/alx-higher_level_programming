05566d332ca251dd0f4448768b54e315c92a088c
jumbamark <jumbamark@yahoo.com>
2022-07-09 07:20:14 +0000

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Module that defines a Base class for other classes in the project.
+"""
+
+
+class Base:
+    """Represents a Base class with a private class attribute.
+    """
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """Initialization of a Base instance
+
+        Args:
+            id: id of the instance
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
