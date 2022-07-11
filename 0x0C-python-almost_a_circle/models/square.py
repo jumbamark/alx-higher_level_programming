e1b5530525de9c6eeb8f1e8b7162763953335277
jumbamark <jumbamark@yahoo.com>
2022-07-11 07:35:41 +0000

    updated the setter method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,5 +32,10 @@ class Square(Rectangle):
     def size(self, value):
         """Sets the size attribute.
         """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
         self.__height = value
