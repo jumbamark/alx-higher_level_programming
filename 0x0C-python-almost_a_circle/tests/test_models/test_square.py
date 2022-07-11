f0ff97be05bef2ae932cb1baa88f41bfb6c84ad5
jumbamark <jumbamark@yahoo.com>
2022-07-11 07:31:36 +0000

    tests for public getter and setter

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -15,7 +15,25 @@ class TestSquare(unittest.TestCase):
         """Method invoked for each test.
         """
         Base._Base__nb_objects = 0
-        self.r1 = Square(5)
+        self.s1 = Square(5)
 
     def test_new_square(self):
-        self.assertEqual(self.r1.id, 1)
+        """Test new Square instances.
+        """
+        self.assertEqual(self.s1.id, 1)
+
+    def test_is_Base_instance(self):
+        """Test Square is a Base instance.
+        """
+        self.assertEqual(True, isinstance(self.s1, Base))
+
+    def test_is_Rectangle_instance(self):
+        """Test Square is a Rectangle instance.
+        """
+        self.assertEqual(True, isinstance(self.s1, Rectangle))
+
+    def test_str(self):
+        """Test __sstr__ representation.
+        """
+        s1 = Square(14, 5, 2, 3)
+        self.assertEqual(str(s1), "[Square] (3) 5/2 - 14")
