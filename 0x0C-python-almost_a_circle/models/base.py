2d643d0e38c6e23d4f2087b99f93803789c4752c
jumbamark <jumbamark@yahoo.com>
2022-07-11 12:23:25 +0000

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """
 Module that defines a Base class for other classes in the project.
 """
+import json
 
 
 class Base:
@@ -22,3 +23,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Method that returns the JSON string representation
+        of list_dictionaries.
+
+        Args:
+            list_dictionaries: list of dictionaries
+        Returns:
+            If "list_dictionaries" is "None" or empty return "[]"
+            Otherwise, return the JSON string representation
+            of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if (type(list_dictionaries) != list or
+           not all(type(x) == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
