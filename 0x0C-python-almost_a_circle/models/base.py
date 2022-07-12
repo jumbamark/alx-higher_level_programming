a9ebbe5c9c0ac346a237a4426e5677414a9931ba
jumbamark <jumbamark@yahoo.com>
2022-07-12 01:05:41 +0000

    JSON ok, but CSV

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ Module that defines a Base class for other classes in the project.
 """
 import json
 import os
+import csv
 
 
 class Base:
@@ -130,3 +131,54 @@ class Base:
             list_ins.append(cls.create(**matrix[index]))
 
         return list_ins
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes list_objs in csv format
+        and saves it to a file
+
+        Args:
+            list_objs: list of instances
+        """
+
+        if (type(list_objs) != list and
+           list_objs is not None or
+           not all(isinstance(x, cls) for x in list_objs)):
+            raise TypeError("list_objs must be a list of instances")
+
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w') as f:
+            if list_objs is not None:
+                list_objs = [x.to_dictionary() for x in list_objs]
+                if cls.__name__ == 'Rectangle':
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    fields = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserializes CSV format from a file.
+
+        Returns:
+                list of instances
+        """
+        filename = cls.__name__ + ".csv"
+        my_list = []
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                reader = csv.reader(f, delimiter=',')
+                if cls.__name__ == 'Rectangle':
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == 'Square':
+                    fields = ['id', 'size', 'x', 'y']
+                for x, row in enumerate(reader):
+                    if x > 0:
+                        i = cls(1, 1)
+                        for j, e in enumerate(row):
+                            if e:
+                                setattr(i, fields[j], int(e))
+                        my_list.append(i)
+        return my_list
