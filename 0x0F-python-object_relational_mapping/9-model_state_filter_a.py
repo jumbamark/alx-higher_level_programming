02f24aa085c1793b7da0d140746e49a65d48c20c
jumbamark <jumbamark@yahoo.com>
2022-09-16 04:27:03 +0000

    Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-lists all State objects that contain
-the letter a from the database hbtn_0e_6_usa
-"""
+"""lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
+
     import sys
-    frommodel_state import Base, State
+    from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
